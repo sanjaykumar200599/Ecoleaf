d1beb4644d751a11ab8a432011638b98cd179c9e
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-02-17 16:59:38 +0530

    fourth commit

--- main_app.py
+++ main_app.py
@@ -6,7 +6,9 @@ from keras.models import load_model
 
 
 #Loading the Model
-model = load_model('"D:\machine_learning\plant disease detection\Plant+Disease+Flask+App\Plant Disease Flask App\Plant_Disease\plant_diseases.h5"')
+model = load_model("D:\machine_learning\plant disease detection\Plant+Disease+Flask+App\Plant Disease Flask App\Plant_Disease\plant_diseases.h5")
+
+
 
 #Name of Classes
 CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
